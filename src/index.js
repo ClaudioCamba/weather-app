a1fd3babb7b1a08e20d93acb6ecf7ab4a17d1686
Claudio Camba <claudio.a.jose@gmail.com>
2022-08-08 11:04:57 +0100

    Add API fetch function to retrieve data from openweather

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import "./style.scss";
 
-console.log("testg");
+const loc = "Islington";
+const key = "e2f88a64edb26908f2c8a836e0f87c19";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&lang=gb&appid=${key}`;
 
-const testing = () => {
-  console.log("TESTING");
-};
+// // Get client location
+// const getLoc
 
-testing();
+// Get location input
+async function getWeatherData(loc) {
+  try {
+    const response = await fetch(loc, { mode: "cors" });
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    alert(err); // TypeError: failed to fetch
+  }
+}
+
+getWeatherData(url);
