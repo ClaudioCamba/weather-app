55965036c8d6f9768dc8b55cf866c7876e69cd71
Claudio Camba <claudio.a.jose@gmail.com>
2022-08-09 15:58:39 +0100

    Complete function to fetch weather data from openweather url, start developing visual for data

--- src/index.js
+++ src/index.js
@@ -1,102 +1,72 @@
 import "./style.scss";
 
-// Get client location
-const inputElem = (() => {
-  const appWrap = document.createElement("div");
-  const elemWrap = document.createElement("div");
-  const input = document.createElement("input");
-  const button = document.createElement("button");
-
-  appWrap.id = "weather-app";
-  elemWrap.className = "input-wrap";
-
-  input.type = "text";
-  input.setAttribute("required", true);
-
-  elemWrap.append(input, button);
-  appWrap.appendChild(elemWrap);
-
-  document.body.appendChild(appWrap);
-
-  // Validate value before making API call
-  button.addEventListener("click", () => {
-    validateInput(input);
-  });
-})();
-
-// Validate input
-const validateInput = (elem) => {
-  if (elem.validity.valid) {
-    getData(elem.value);
-  } else {
-    console.log("invalid");
-  }
-};
-
-// function doubleAfter2Seconds(x) {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve(x * 2);
-//     }, 100);
-//   });
-// }
-
-// async function addAsync(x) {
-//   const a = await doubleAfter2Seconds(10);
-//   const b = await doubleAfter2Seconds(20);
-//   const c = await doubleAfter2Seconds(30);
-//   return x + a + b + c;
-// }
-
-// console.log(
-//   addAsync(10).then((sum) => {
-//     console.log(sum);
-//   })
-// );
-
-// Calling and cleaning data
-const getData = (val) => {
+const weatherApp = (() => {
+  // Store values
   const values = {
-    location: val,
+    loc: null,
     limit: 1,
+    units: "&units=metric",
     apiKey: "e2f88a64edb26908f2c8a836e0f87c19",
     coordUrl: "https://api.openweathermap.org/geo/1.0/direct?q=",
-    coord: "",
-    foreUrl: "https://api.openweathermap.org/data/2.5/forecast/daily?lat=",
-    fore: "",
+    coord: null,
+    todayUrl: "https://api.openweathermap.org/data/2.5/weather?lat=",
+    today: null,
+    foreUrl: "https://api.openweathermap.org/data/2.5/forecast?lat=",
+    fore: null,
   };
 
-  // requestData(values.coordUrl + values.location + "&appid=" + values.apiKey).then(function (result) {
-  //   values.coord = result;
-  //   console.log(result);
-  //   // requestData(values.foreUrl + result[0].lat + "&lon=" + result[0].lon + "&cnt=7&appid=" + values.apiKey).then(function (result) {
-  //   //   console.log(result);
-  //   // });
-  // });
-  // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-  requestData("https://api.openweathermap.org/data/2.5/forecast?lat=51.5073219&lon=-0.1276474&appid=e2f88a64edb26908f2c8a836e0f87c19").then(
-    function (result) {
-      console.log(result);
+  // Input element to get client location
+  const inputElem = (() => {
+    const appWrap = document.querySelector("#weather-app");
+    const elemWrap = document.createElement("div");
+    const input = document.createElement("input");
+    const button = document.createElement("button");
+
+    elemWrap.className = "input-wrap";
+    input.type = "text";
+    input.pattern = "[a-z-A-Z]+";
+    input.required = true;
+
+    elemWrap.append(input, button);
+    appWrap.querySelector(".title-search").appendChild(elemWrap);
+
+    // Validate value before making API call
+    button.addEventListener("click", () => {
+      if (input.validity.valid) {
+        values.loc = input.value;
+        getData(values.loc);
+      } else {
+        console.log("invalid");
+        console.log(input.validity);
+      }
+    });
+  })();
+
+  // Fetch weather Data
+  const getData = (val) => {
+    requestData(values.coordUrl + val + "&appid=" + values.apiKey).then((coord) => {
+      values.coord = coord[0];
+      // Current fetch
+      requestData(values.todayUrl + coord[0].lat + "&lon=" + coord[0].lon + "&appid=" + values.apiKey + values.units).then((fore) => {
+        console.log("current");
+        console.log(fore);
+      });
+      // Forecast fetch
+      requestData(values.foreUrl + coord[0].lat + "&lon=" + coord[0].lon + "&appid=" + values.apiKey + values.units).then((fore) => {
+        console.log("forecast");
+        console.log(fore);
+      });
+    });
+  };
+
+  // Fetch request function
+  async function requestData(url) {
+    try {
+      const response = await fetch(url, { mode: "cors" });
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      alert(err); // TypeError: failed to fetch
     }
-  );
-};
-
-// Get location input
-async function requestData(url) {
-  let data;
-  try {
-    const response = await fetch(url, { mode: "cors" });
-    return (data = await response.json());
-  } catch (err) {
-    alert(err); // TypeError: failed to fetch
   }
-}
-
-// Sort Data
-const sortData = (data) => {
-  console.log(data);
-};
-
-// getWeatherData(url);
-// 51.5073219
-// -0.1276474
+})();
