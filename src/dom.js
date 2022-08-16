4b7593491587ebc5f14e45a8bc36481cf96fc7a6
Claudio Camba <claudio.a.jose@gmail.com>
2022-08-16 16:49:50 +0100

    Connect API data to min, max and feels like temperature

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,4 @@
-import { location, temperature, description } from "./variables.js";
+import { location, temperature, description, minTemp, maxTemp, feelsLike } from "./variables.js";
 
 const domManipulation = (function () {
   "use strict";
@@ -6,17 +6,33 @@ const domManipulation = (function () {
     _location(data);
     _CurrentTemp(data);
     _Description(data);
+    _minTemp(data);
+    _maxTemp(data);
+    _feelsLike(data);
   };
 
+  // DOM functions
   const _location = (data) => {
     location.innerText = data.name + ", " + data.sys.country;
   };
   const _CurrentTemp = (data) => {
-    temperature.innerText = Math.round(data.main.temp);
+    temperature.innerHTML = formatTemp(Math.round(data.main.temp));
   };
   const _Description = (data) => {
     description.innerText = data.weather[0].description;
   };
+  const _minTemp = (data) => {
+    minTemp.innerHTML = "Min Temp: " + formatTemp(Math.round(data.main.temp_min));
+  };
+  const _maxTemp = (data) => {
+    maxTemp.innerHTML = "Max Temp: " + formatTemp(Math.round(data.main.temp_max));
+  };
+  const _feelsLike = (data) => {
+    feelsLike.innerHTML = "Feels like: " + formatTemp(Math.round(data.main.feels_like));
+  };
+
+  // Helper functions
+  const formatTemp = (temp) => temp + '<span class="deg-symbol">&#176;<span class="minus">-</span></span>C';
 
   return { updateCurrent };
 })();
--- src/variables.js
+++ src/variables.js
@@ -1,7 +1,10 @@
 "use strict";
 
 const location = document.querySelector(".intro .location");
-const temperature = document.querySelector(".temp-wrap .num");
+const temperature = document.querySelector(".temp-wrap .temp");
 const description = document.querySelector(".temp-wrap .desc");
+const minTemp = document.querySelector(".desc-1 .min-temp");
+const maxTemp = document.querySelector(".desc-1 .max-temp");
+const feelsLike = document.querySelector(".desc-1 .feels-like");
 
-export { location, temperature, description };
+export { location, temperature, description, minTemp, maxTemp, feelsLike };
